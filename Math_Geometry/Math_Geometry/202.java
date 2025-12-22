1530eb8e3ef1cc5da31d1b288f1e81d954b44231
Leela0o5 <leela592023@gmail.com>
2025-12-22 09:43:02 +0530

    Changes

---
+++ Math_Geometry/Math_Geometry/202.java
@@ -0,0 +1,21 @@
+package Math_Geometry;
+class Solution {
+    public boolean isHappy(int n) {
+        if(n ==1 || n==7){
+            return true;
+        }
+        while(n>9){
+            int sum = 0;
+            while(n>0){
+                int ld = n%10;
+                sum += (ld*ld);
+                n/=10;
+            }
+            n = sum;
+           
+            }
+
+
+return n ==1 || n==7;
+    }
+}
